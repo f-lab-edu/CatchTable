2e235a5d1f05abe5b4ee28fd80e90ac296055d44
dainn <dainn1992@naver.com>
2023-07-23 23:05:23 +0900

    fixed test codes

--- src/app/entrypoints/dependencies.py
+++ src/app/entrypoints/dependencies.py
@@ -1,5 +1,3 @@
-from fastapi.security import OAuth2PasswordBearer
-from passlib.context import CryptContext
 from app.service_layer import unit_of_work
 from app import config
 from app.domain import model, schemas
@@ -12,20 +10,3 @@ def get_uow():
     model.Base.metadata.create_all(bind=Engine)
     return unit_of_work.SqlAlchemyUnitOfWork(sessionmaker(bind=Engine))
 
-
-
-
-# openssl rand -hex 32
-SECRET_KEY = "9416c7e82785973d4c1971075f1dcefeb91621c4662eb1be0d1b87f1777399f7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
