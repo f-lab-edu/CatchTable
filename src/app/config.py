f18dd507c853c5361d80b01a89bdfc1807a46fa6
dainn <dainn1992@naver.com>
2023-08-31 18:13:08 +0900

    pydantic config for postgreSQL

--- src/app/config.py
+++ src/app/config.py
@@ -1,9 +1,18 @@
 import os
+from pydantic import BaseSettings
 
+class Settings(BaseSettings):
+    DB_USERNAME: str = os.getenv("DATABASE_USERNAME")
+    DB_PASSWORD: str = os.getenv("DATABASE_PASSWORD")
+    DB_HOST: str = os.getenv("DATABASE_HOST", default="localhost")
+    DB_PORT: str = os.getenv("DATABASE_PORT", default=5432)
+    DB_DATABASE: str = os.getenv("DATABASE_NAME")
 
+    DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
+
+settings = Settings()
 def get_postgres_uri():
-    SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
-    if SQLALCHEMY_DATABASE_URL is None:
+    if settings.DATABASE_URL is None:
         raise ValueError("DATABASE_URL environment variable not set")
+    return settings.DATABASE_URL
 
-    return SQLALCHEMY_DATABASE_URL
